0a0df0634555ec4776860be8289d8b6a6cb36c6e
Denyse <mdenyse15@gmail.com>
2022-06-08 20:40:15 +0200

    ft: added update function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 
-const createUser = async (req,res) => {
+const createUser = async (req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -8,21 +8,57 @@ const createUser = async (req,res) => {
         gender: req.body.gender
     });
     return await user.save().then(() => {
-        res.status(201).json({message: 'User registered successfully', data: user})
+        res.status(201).json({
+            message: 'User registered successfully',
+            data: user
+        })
     }).catch((err) => {
-        res.status(400).json({message: 'Error registering user', error: err.message})
+        res.status(400).json({
+            message: 'Error registering user',
+            error: err.message
+        })
     })
 }
 
-const getUsers = async (req,res) => {
+const getUsers = async (req, res) => {
     return await User.find().then((users) => {
-        res.status(200).json({message: 'Users retrieved successfully', data: users})
+        res.status(200).json({
+            message: 'Users retrieved successfully',
+            data: users
+        })
     }).catch((err) => {
-        res.status(400).json({message: 'Error retrieving users', error: err.message})
+        res.status(400).json({
+            message: 'Error retrieving users',
+            error: err.message
+        })
     });
 }
 
+const updateUser = async (req, res) => {
+    const filter = {
+        _id: req.params.id
+    }
+    return await User.findOneAndUpdate(filter, {
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.gender
+    }, {
+        new: true
+    }).then((users) => {
+        res.status(200).json({
+            message: 'Users updated successfully',
+            data: users
+        })
+    }).catch((err) => {
+        res.status(400).json({
+            message: 'Error editing users',
+            error: err.message
+        })
+    });;
+}
+
 export {
-    createUser ,
-    getUsers
+    createUser,
+    getUsers,
+    updateUser
 }
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import { createUser, getUsers } from '../controllers/user.controller.js'
+import { createUser, getUsers, updateUser } from '../controllers/user.controller.js'
 
 router.post('/', createUser);
 
 router.get('/', getUsers) ;
+
+router.put('/:id', updateUser) ;
 
 export default router;
